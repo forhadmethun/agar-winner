7b3a9b81c21dae5b0286b452ba35e0056ef890ca
forhad <forhadmethun@gmail.com>
2023-05-19 15:29:20 +0300

    Update project structure

---
+++ src/main/scala/core/GameStreamUpdater.scala
@@ -0,0 +1,25 @@
+package core
+
+import cats.effect.*
+import fs2.Stream
+import org.http4s.websocket.WebSocketFrame
+import io.circe.syntax.*
+import models.Response
+import services.PlayerService
+import scala.concurrent.duration.*
+
+final class GameStreamUpdater[F[_]](val playerService: PlayerService[F]):
+  def updatedPlayerList(using Async[F]): Stream[F, F[WebSocketFrame]] =
+    Stream
+      .awakeEvery[F](33.milliseconds)
+      .flatMap(_ => Stream.eval(playerService.getAllPlayers))
+      .map(players => {
+        Sync[F]
+          .pure(WebSocketFrame.Text(Response.PlayerListMessageResponse(
+            players.map(_.playerData)).asInstanceOf[Response].asJson.toString))
+      })
+
+object GameStreamUpdater:
+  def create[F[_]: Async](playerService: PlayerService[F]): GameStreamUpdater[F] =
+    new GameStreamUpdater[F](playerService)
+    
