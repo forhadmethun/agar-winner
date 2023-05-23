fabd888899862865445fa5a8b4710a66a217db02
forhad <forhadmethun@gmail.com>
2023-05-23 10:18:31 +0300

    Update the tests of PlayerServiceTest.scala with scalacheck's Gen

--- src/test/scala/services/PlayerServiceTest.scala
+++ src/test/scala/services/PlayerServiceTest.scala
@@ -2,95 +2,113 @@ package services
 
 import cats.effect.IO
 import models.OrbData
-import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 import munit.FunSuite
 import repository.PlayerRepository
 import models.*
-
+import org.scalacheck.Gen
+import org.scalacheck.Prop.forAll
 class PlayerServiceTest extends FunSuite {
+  val playerConfigGen: Gen[PlayerConfig] = for {
+    speed <- Gen.posNum[Double]
+    zoom <- Gen.posNum[Double]
+  } yield PlayerConfig(speed = speed, zoom = zoom)
+
+  val playerDataGen: Gen[PlayerData] = for {
+    playerId <- Gen.alphaNumStr
+    playerName <- Gen.alphaNumStr
+    sid <- Gen.alphaNumStr
+    locX <- Gen.posNum[Double]
+    locY <- Gen.posNum[Double]
+    color <- Gen.alphaStr
+    radius <- Gen.posNum[Double]
+  } yield PlayerData(playerId, sid, playerName, locX, locY, color, radius)
+
   test("getAllPlayers should return all players") {
-    val playerConfig = PlayerConfig(speed = 0, zoom = 0.0)
-    val playerData = PlayerData("player1", "sid1", "John", 10.0, 10.0, "blue", 10.0)
-    val player1 = Player[IO](playerConfig, playerData)
-    val repo = new PlayerRepository[IO] {
-      override def getAll: IO[Vector[Player[IO]]] = IO.pure(Vector(player1));
-      override def get(uid: String): IO[Option[Player[IO]]] = ???
-      override def update(player: Player[IO]): IO[Unit] = ???
-      override def delete(uid: String): IO[Unit] = ???
+    val getAllPlayersProp = forAll(Gen.listOfN(5, Gen.zip(playerConfigGen, playerDataGen))) { playersList =>
+      val players = playersList.map { case (playerConfig, playerData) => Player[IO](playerConfig, playerData) }
+      val repo = new PlayerRepository[IO] {
+        override def getAll: IO[Vector[Player[IO]]] = IO.pure(players.toVector)
+        override def get(uid: String): IO[Option[Player[IO]]] = ???
+        override def update(player: Player[IO]): IO[Unit] = ???
+        override def delete(uid: String): IO[Unit] = ???
+      }
+      val service = new PlayerService[IO](repo)
+      val result = service.getAllPlayers.unsafeRunSync()
+      result == players.toVector
     }
-    val service = new PlayerService[IO](repo)
-    val result = service.getAllPlayers.unsafeRunSync()
-    val expectedPlayers = Vector(player1);
-    assertEquals(result, expectedPlayers)
+    getAllPlayersProp.check()
   }
 
   test("getPlayer should return a player with the given ID") {
-    val playerConfig = PlayerConfig(speed = 0, zoom = 0.0)
-    val playerData = PlayerData("player1", "sid1", "John", 10.0, 10.0, "blue", 10.0)
-    val player = Player[IO](playerConfig, playerData)
-    val repo = new PlayerRepository[IO] {
-      override def get(uid: String): IO[Option[Player[IO]]] =
-        if (uid == playerData.uid) IO.pure(Some(player))
-        else IO.pure(None)
-      override def getAll: IO[Vector[Player[IO]]] = ???
-      override def update(player: Player[IO]): IO[Unit] = ???
-      override def delete(uid: String): IO[Unit] = ???
-    }
+    val getPlayerProp = forAll(playerConfigGen, playerDataGen) { (playerConfig, playerData) =>
+      val player = Player[IO](playerConfig, playerData)
+      val repo = new PlayerRepository[IO] {
+        override def get(uid: String): IO[Option[Player[IO]]] =
+          if (uid == playerData.uid) IO.pure(Some(player)) else IO.pure(None)
+        override def getAll: IO[Vector[Player[IO]]] = ???
+        override def update(player: Player[IO]): IO[Unit] = ???
+        override def delete(uid: String): IO[Unit] = ???
+      }
 
-    val service = new PlayerService[IO](repo)
-    val result = service.getPlayer(playerData.uid).unsafeRunSync()
-    assertEquals(result, player)
+      val service = new PlayerService[IO](repo)
+      val result = service.getPlayer(playerData.uid).unsafeRunSync()
+      result == player
+    }
+    getPlayerProp.check()
   }
 
   test("getPlayer should throw an exception when no player is found with the given ID") {
-    val playerId = "123"
-
-    val repo = new PlayerRepository[IO] {
-      override def get(uid: String): IO[Option[Player[IO]]] = IO.pure(None)
-      override def getAll: IO[Vector[Player[IO]]] = ???
-      override def update(player: Player[IO]): IO[Unit] = ???
-      override def delete(uid: String): IO[Unit] = ???
+    val playerIdGen: Gen[String] = Gen.alphaNumStr
+    val getPlayerProp = forAll(playerIdGen) { playerId =>
+      val repo = new PlayerRepository[IO] {
+        override def get(uid: String): IO[Option[Player[IO]]] = IO.pure(None)
+        override def getAll: IO[Vector[Player[IO]]] = ???
+        override def update(player: Player[IO]): IO[Unit] = ???
+        override def delete(uid: String): IO[Unit] = ???
+      }
+      val service = new PlayerService(repo)
+      val result = service.getPlayer(playerId).attempt.unsafeRunSync()
+      result.isLeft && result.isInstanceOf[Left[Throwable, _]] &&
+        result.swap.toOption.get.isInstanceOf[IllegalArgumentException] &&
+        result.swap.toOption.get.getMessage == s"No player found with id: $playerId"
     }
-
-    val service = new PlayerService(repo)
-    val result = service.getPlayer(playerId).attempt.unsafeRunSync()
-    assert(result.isLeft)
-    assert(result.isInstanceOf[Left[Throwable, _]])
-    assert(result.swap.toOption.get.isInstanceOf[IllegalArgumentException])
-    assertEquals(result.swap.toOption.get.getMessage, s"No player found with id: $playerId")
+    getPlayerProp.check()
   }
 
   test("savePlayer should return the saved player") {
-    val playerConfig = PlayerConfig(speed = 0, zoom = 0.0)
-    val playerData = PlayerData("player1", "sid1", "John", 10.0, 10.0, "blue", 10.0)
-    val repo = new PlayerRepository[IO] {
-      override def update(player: Player[IO]): IO[Unit] = IO.unit
-      override def get(uid: String): IO[Option[Player[IO]]] = ???
-      override def getAll: IO[Vector[Player[IO]]] = ???
-      override def delete(uid: String): IO[Unit] = ???
+    val savePlayerProp = forAll(playerConfigGen, playerDataGen) { (playerConfig, playerData) =>
+      val repo = new PlayerRepository[IO] {
+        override def update(player: Player[IO]): IO[Unit] = IO.unit
+        override def get(uid: String): IO[Option[Player[IO]]] = ???
+        override def getAll: IO[Vector[Player[IO]]] = ???
+        override def delete(uid: String): IO[Unit] = ???
+      }
+      val service = new PlayerService(repo)
+      val result = service.savePlayer(playerConfig, playerData).unsafeRunSync()
+      result.playerConfig == playerConfig && result.playerData == playerData
     }
-    val service = new PlayerService(repo)
-    val result = service.savePlayer(playerConfig, playerData).unsafeRunSync()
-    assertEquals(result.playerConfig, playerConfig)
-    assertEquals(result.playerData, playerData)
+    savePlayerProp.check()
   }
 
   test("deletePlayer should delete the player") {
-    val playerId = "123"
-    var deletedPlayerId: Option[String] = None
-    val repo = new PlayerRepository[IO] {
-      override def delete(uid: String): IO[Unit] = {
-        deletedPlayerId = Some(uid)
-        IO.unit
+    val playerIdGen: Gen[String] = Gen.alphaNumStr
+    val playerIdProp = forAll(playerIdGen) { playerId =>
+      var deletedPlayerId: Option[String] = None
+      val repo = new PlayerRepository[IO] {
+        override def delete(uid: String): IO[Unit] = {
+          deletedPlayerId = Some(uid)
+          IO.unit
+        }
+        override def get(uid: String): IO[Option[Player[IO]]] = ???
+        override def getAll: IO[Vector[Player[IO]]] = ???
+        override def update(player: Player[IO]): IO[Unit] = ???
       }
-      override def get(uid: String): IO[Option[Player[IO]]] = ???
-      override def getAll: IO[Vector[Player[IO]]] = ???
-      override def update(player: Player[IO]): IO[Unit] = ???
+      val service = new PlayerService(repo)
+      service.deletePlayer(playerId).unsafeRunSync()
+      deletedPlayerId.contains(playerId)
     }
-    val service = new PlayerService(repo)
-    service.deletePlayer(playerId).unsafeRunSync()
-    assertEquals(deletedPlayerId, Some(playerId))
+    playerIdProp.check()
   }
 
   test("create should return a PlayerService instance") {
